b0a8d49486bac21a02c5a2e361f02371110646cb
Bahadir HG <bahadirhamzagul@gmail.com>
2020-12-06 13:43:34 +0300

    Update esp.ino
    Commenting

--- esp.ino
+++ esp.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+
 const char* ssid = "Bahadir";
 const char* password = "bahadir234";
 String readString;
@@ -10,6 +11,7 @@ void setup () {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
+  //Wait while connecting to wifi 
   while (WiFi.status() != WL_CONNECTED) {
 
     delay(1000);
@@ -20,58 +22,52 @@ void setup () {
 
 }
 
+
+
+
 void loop() {
 
-
 while (Serial.available()) {
-    delay(3);  
+  
+    delay(3);  //Okumanın daha güvenilir olması için 3mslik delay
     char c = Serial.read();
     readString += c; 
   }
 
   if (readString.length() >0) {
-  
     
     readString.trim();
-    denemeCall(readString);
+    httpCall(readString);
     readString="";
     
    }
-
-
-
 }
 
+void httpCall(String url){
+if (WiFi.status() == WL_CONNECTED) { //WiFi durumunu kontrol et
 
+        HTTPClient http;  
 
-
-
-void denemeCall(String url){
-if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-
-        HTTPClient http;  //Declare an object of class HTTPClient
-
-        http.begin("http://fuzzysera.herokuapp.com:80/"+url);  //Specify request destination
-        int httpCode = http.GET();                                                                  //Send the request
+        http.begin("http://mysera.herokuapp.com:80/"+url);  
+        int httpCode = http.GET();                                                               
         
-        if (httpCode == 200) { //Check the returning code
+        if (httpCode == 200) { //Status code'u kontrol et
 
-           String payload = http.getString();
-                  //Get the request response payload
-           Serial.println(payload);                     //Print the response payload
+           String payload = http.getString();           
+           Serial.println(payload);                  //Veriyi al ve arduinoya yolla   
 
         }else{
-           Serial.println("Hata :"+String(httpCode)); 
+           Serial.println("Hata :"+String(httpCode));  //Kod 200 değilse gelen kodu arduinoya yolla
         }
 
-       http.end();   //Close connection
+       http.end();   
 
      }
 
      return;
 }
   
-
+//Veri varsa alarak serial portu temizle
 void flushRecieve(){
       while(Serial.available())
         Serial.read();
